e671a53786e905bb6c61248a4c45c04643b9b657
biet-ian <nitinkv@outlook.com>
2020-07-30 17:04:57 +0530

    add test case for special alerts

--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -19,6 +19,24 @@ TEST(VitalsTest, when_a_vital_is_off_limit_it_is_reported_with_vital_id) {
     ASSERT_EQ(results[respRate], true);
 }
 
+TEST(VitalsTest, alert_spo2_low_resprate_high ) { 
+    std::vector<Measurement> measurements = {
+        {bpm, 100},
+        {spo2, 50},
+        {respRate, 70},
+    };
+    auto result = vitalAlert(measurements);
+    ASSERT_EQ(result, VITAL_ALERT::lung_compromised );
+
+    std::vector<Measurement> measurements = {
+        {bpm, 100},
+        {spo2, 50},
+        {respRate, 50},
+    };
+    auto result = vitalAlert(measurements);
+    ASSERT_EQ(result, VITAL_ALERT::no_special_alert );
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
